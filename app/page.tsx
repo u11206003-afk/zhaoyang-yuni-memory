19130292a7309dbc8780a689091cf3cc67801c4c
u11206003-afk <u11206003@go.utaipei.edu.tw>
2026-08-26 16:16:48 +0800

    Add app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { requireChatGPTUser } from './chatgpt-auth';
+import MemoryApp from './MemoryApp';
+
+export const dynamic = 'force-dynamic';
+
+export default async function HomePage() {
+  await requireChatGPTUser('/');
+  return <MemoryApp />;
+}
+
